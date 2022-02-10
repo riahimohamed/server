8028a74ec79a5f10e702c7b313bab6f66d56def4
riahimohamed <riahimohammed7@gmail.com>
2022-02-10 11:03:15 +0100

    Revert "update add cart model"
    This reverts commit c961ce145198f5f85ad74842cac7ec89e64b4c94.

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ require("./route/authenticate/twitter");
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(session(
-	{ secret: 'secret',
+app.use(require('express-session')(
+	{ secret: 'keyboard cat',
 	  resave: false, 
 	  saveUninitialized: false }));
 
@@ -28,9 +28,7 @@ const users = require('./route/user.route');
 app.use('/users', users);
 
 const products = require('./route/products.route');
-const cart = require('./route/cart.route');
 app.use('/products', products);
-app.use('/cart', cart);
 
 const category = require('./route/category.route');
 app.use('/category', category);
